ed0caf572776cf7d91fe81aeaf5f2269db2a8e48
grassblock <hi@imgb.space>
2025-05-03 12:08:21 +0800

    feat: add mdx & sitemap support (for real)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://terminal-blog.example.com',
   base: '/',
   trailingSlash: 'ignore',
+
   build: {
     format: 'directory'
   },
+
   markdown: {
     shikiConfig: {
       theme: 'nord',
       wrap: true
     }
-  }
+  },
+
+  integrations: [sitemap(), mdx()]
 });
