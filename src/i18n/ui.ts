7af0886d9cc9cd05ca87aa9bb94c637ac3bf31f2
grassblock <i@gb0.dev>
2025-07-20 21:56:57 +0800

    feat: init i18n features

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    zh_hans: '中文（简体）',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.twitter': 'Twitter',
+    },
+    zh_hans: {
+        'nav.home': '首页',
+        'nav.about': '关于',
+    },
+} as const;
