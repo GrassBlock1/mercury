666349c5b8543382267391618145315270a2bd4c
grassblock <i@gb0.dev>
2025-08-10 18:37:37 +0800

    feat: display last modified and wordcount on article

--- src/plugins/remark/modified-time.mjs
+++ src/plugins/remark/modified-time.mjs
@@ -1,10 +1,9 @@
-import { execSync } from "child_process";
-import { statSync } from "node:fs";
+import {execSync} from "child_process";
+import {statSync} from "node:fs";
 
 export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0];
-        const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString() || statSync(filepath).mtime.toISOString();
-        file.data.astro.frontmatter.lastModified = result.toString();
+        file.data.astro.frontmatter.lastModified = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString() || statSync(filepath).mtime.toISOString();
     };
 }
