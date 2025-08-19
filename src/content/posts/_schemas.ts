81ddec2e15ade7fbf68956054ada47395c6b233f
grassblock <i@gb0.dev>
2025-08-19 21:16:09 +0800

    feat: add optional 'updated' front matter for more precise updated time

--- src/content/posts/_schemas.ts
+++ src/content/posts/_schemas.ts
@@ -1,14 +1,15 @@
-import { z, reference } from 'astro:content';
+import {z, reference} from 'astro:content';
 
 // @ts-ignore
-export const posts = ({ image }) => z.object({
-  title: z.string(),
-  description: z.string().optional(),
-  draft: z.boolean().optional().default(false),
-  summary: z.string().optional(),
-  date: z.coerce.date(),
-  categories: z.union([z.array(z.string()), z.string()]).transform(val => Array.isArray(val) ? val : [val]).default(['uncategorized']),
-  tags: z.array(z.string()).optional(),
-  cover: image().optional(),
-  author: z.union([z.array(reference('authors')), reference('authors')]).optional(),
+export const posts = ({image}) => z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    draft: z.boolean().optional().default(false),
+    summary: z.string().optional(),
+    date: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+    categories: z.union([z.array(z.string()), z.string()]).transform(val => Array.isArray(val) ? val : [val]).default(['uncategorized']),
+    tags: z.array(z.string()).optional(),
+    cover: image().optional(),
+    author: z.union([z.array(reference('authors')), reference('authors')]).optional(),
 });
