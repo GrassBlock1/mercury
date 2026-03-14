486c06f5e8776b1014d888938fff2ea8b1c86f2f
grassblock <i@gb0.dev>
2026-03-14 18:05:41 +0800

    feat: add a helper function to get all languages in routes

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,28 @@
 import {ui, defaultLang, showDefaultLang} from './ui';
+import {i18n} from "astro:config/client";
+
+// types from Astro config
+type LocaleConfig = string | { path: string; codes: string[] };
+type Locales = LocaleConfig[];
+
+
+export function getLocalePathMap(locales: Locales = i18n?.locales ?? []): Record<string, string> {
+    const result: Record<string, string> = {};
+    
+    for (const locale of locales) {
+        if (typeof locale === 'string') {
+            // simple
+            result[locale] = locale;
+        } else {
+            // reflect the same path to multiple languages
+            for (const code of locale.codes) {
+                result[code] = locale.path;
+            }
+        }
+    }
+    
+    return result;
+}
 
 export function getLangFromUrl(url: URL) {
     const [, lang] = url.pathname.split('/');
@@ -7,8 +31,10 @@ export function getLangFromUrl(url: URL) {
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-    return function t(key: keyof typeof ui[typeof defaultLang]) {
-        return ui[lang][key] || ui[defaultLang][key];
+    return function t(key: string) {
+        const langTranslations = ui[lang] as Record<string, string>;
+        const defaultTranslations = ui[defaultLang as keyof typeof ui] as Record<string, string>;
+        return langTranslations[key] || defaultTranslations[key];
     }
 }
 
