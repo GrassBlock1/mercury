89ec81a093fbfb9bd0fba3239f070f9491bcaa19
grassblock <i@gb0.dev>
2026-03-25 21:56:03 +0800

    fix: search result link slug when i18n is configured

--- src/pages/search-index.json.js
+++ src/pages/search-index.json.js
@@ -1,15 +1,19 @@
 import { getCollection } from 'astro:content';
+import {defaultLocale, locales} from "@/i18n/utils.ts";
+import {getPathByLocale} from "astro:i18n";
 
 export async function GET() {
   const posts = await getCollection('posts', ({ data }) => {
     return import.meta.env.PROD ? data.draft !== true : true;
 });
+  const languages = locales.filter(item => item !== defaultLocale)
   const searchIndex = posts.map(post => ({
     title: post.data.title,
     description: post.data.description || '',
     content: post.body,
     date: post.data.date,
-    slug: post.slug
+    lang: languages.includes(post.filePath.split("/")[0]) ? getPathByLocale(post.filePath.split("/")[0]) : undefined,
+    slug: languages.includes(post.filePath.split("/")[0]) ? post.id.split("/").slice(1).join("/") : post.id
   }));
 
   return new Response(JSON.stringify(searchIndex), {
